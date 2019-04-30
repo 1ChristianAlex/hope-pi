a63151f1f86339b46b798c4f8d0a95bd1a51b9e4
Christian Alexsander <chirstian@bertholdo.com.br>
2019-04-30 15:45:49 -0300

    working on promise for each?

--- src/app/services/fire-base-service.service.ts
+++ src/app/services/fire-base-service.service.ts
@@ -16,20 +16,31 @@ export class FireBaseServiceService {
   public async getAgain(){
     return await this.fBase.object('firebase').update({testez:{testea:123,firefromIonic:true}})
   }
-  public getFiles():Promise<any>{
-    return new Promise((res,rej)=>{
-      try {
-        let filesA = []
-        this.fBase.list('articles/articlesFiles').snapshotChanges().subscribe(item=>{
-          item.map((map)=>{
-            filesA.push(this.fStorage.ref(`articleFiles/${map.payload.val()}`).getMetadata())
-          })
+  public async getFiles(){
+    try {
+      let nameArrayPromise = new Promise((res,rej)=>{
+        this.fBase.list('articles/articlesFiles').snapshotChanges().subscribe(names=>{
+          let namesSub = []
+          names.map(nome=>{
+            namesSub.push(nome.payload.val())
+          });
+          res(namesSub)
         });
-        res(filesA)
-      } catch (error) {
-        rej(error)
-      }
-    })
-    
+      })
+      
+      let arrayName:Array<any> = (await nameArrayPromise as Array<any>);
+      let filesArrayPromise = new Promise((res,rej)=>{
+        let filesArray = []
+        arrayName.forEach(name=>{
+          this.fStorage.ref(`articleFiles/${name}`).getMetadata().subscribe(file=>{
+            console.log(file)
+          })
+        })
+        res(filesArray)
+      })
+      
+    } catch (error) {
+      
+    }
   }
 }
